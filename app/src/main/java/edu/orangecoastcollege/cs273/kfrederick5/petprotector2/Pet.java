e9bbba31d2e233d4d1383f1e66b660314d7eece1
kfrederick5 <tefsuncle@gmail.com>
2016-10-27 16:33:04 -0700

    Added gets and sets

--- app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5/petprotector2/Pet.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5/petprotector2/Pet.java
@@ -5,4 +5,55 @@ package edu.orangecoastcollege.cs273.kfrederick5.petprotector2;
  */
 
 public class Pet {
+    private String mDetails;
+    private int mId;
+    private String mImageURI;
+    private String mName;
+    private int mPhone;
+
+
+    public String getmDetails() {
+        return mDetails;
+    }
+
+    public int getmId() {
+        return mId;
+    }
+
+    public String getmImageURI() {
+        return mImageURI;
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public int getmPhone() {
+        return mPhone;
+    }
+
+    public void setmDetails(String mDetails) {
+        this.mDetails = mDetails;
+    }
+
+    public void setmId(int mId) {
+        this.mId = mId;
+    }
+
+    public void setmImageURI(String mImageURI) {
+        this.mImageURI = mImageURI;
+    }
+
+    public void setmName(String mName) {
+        this.mName = mName;
+    }
+
+    public void setmPhone(int mPhone) {
+        this.mPhone = mPhone;
+    }
+
+    public void Pet(String name, String details, int phone)
+    {
+
+    }
 }
