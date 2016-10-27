68588c7be09d0f963dceb4e0bc68489c5d166781
Keegan Frederick <tefsuncle@hotmail.com>
2016-10-27 13:41:51 -0700

    Initial commit

---
+++ app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5/petprotector2/Pet.java
@@ -0,0 +1,8 @@
+package edu.orangecoastcollege.cs273.kfrederick5.petprotector2;
+
+/**
+ * Created by Link on 10/27/2016.
+ */
+
+public class Pet {
+}
---
+++ app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5/petprotector2/PetListAdapter.java
@@ -0,0 +1,13 @@
+package edu.orangecoastcollege.cs273.kfrederick5.petprotector2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PetListAdapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pet_list_adapter);
+    }
+}
